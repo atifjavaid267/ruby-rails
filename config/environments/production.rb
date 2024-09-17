2d0cf359aaef9aed21ffff6fbcfc25f0866da780
Atif Javaid <70339749+atifjavaid267@users.noreply.github.com>
2024-09-17 13:34:02 -0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,9 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
-
+  config.hosts << "https://railsproduction7599.up.railway.app/"
+  config.hosts << "www.railsproduction7599.up.railway.app"
+  config.hosts << "railsproduction7599.up.railway.app"
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
